d32b1f379e6d7ceb93f76386b645f1697d26aa4c
Astavie <astavie@protonmail.com>
2021-08-04 15:36:37 +0200

    close div tag

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sveltex.MixProject do
   def project do
     [
       app: :sveltex,
-      version: "0.1.2",
+      version: "1.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       description: description(),
